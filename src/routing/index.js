53d34ca17e3ad6bc845e434aa0b74b6711c08377
vikkichild <vika@child.com>
2019-09-01 14:49:49 +0300

    fix: fixed route logic

--- src/routing/index.js
+++ src/routing/index.js
@@ -3,9 +3,7 @@
 const express = require('express');
 const router = express.Router();
 const companyController = require('../controllers/companyController');
-const companyPaginationController = require('../controllers/companyPaginationController');
 
 router.get('/companies', companyController.get);
-router.get('/companies/?page=:page&per_page=:per_page', companyPaginationController.get);
 
 module.exports = router;
