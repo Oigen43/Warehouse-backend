ce3d9ecda4fce5c0adfbd1731341c9282c230957
vikkichild <vikkichild137@gmail.com>
2019-09-10 09:55:39 +0300

    refactor: added helmet middleware

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const logger = require('./utils/logger');
 const cors = require('cors');
+const helmet = require('helmet');
 
 const router = require('./routing');
 
@@ -12,11 +13,10 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(cors());
+app.use(helmet());
 
 app.use('/', router);
-app.disable('x-powered-by');
 
 const DEFAULT_PORT = 3000;
 const port = parseInt(process.env.PORT, 10) || DEFAULT_PORT;
