0d70976828e427a14efe746bedd30b30981e1796
Oigen43 <oigen43@icloud.com>
2019-09-24 10:18:18 +0300

    feat: add permissions check for warehouses and companies

--- src/routing/companyRoutes/index.js
+++ src/routing/companyRoutes/index.js
@@ -5,13 +5,16 @@ const expressJoi = require('express-joi-validator');
 const passport = require('passport');
 const companyQuerySchema = require('./ValidatorSchema');
 const companyController = require('../../controllers/companyController');
+const permissionsCheck = require('../../utils/permissionsValidator');
+const routesPermissions = require('../../const/routesPermissions');
+
 
 const router = express.Router();
 
 router.route('/')
-    .get(expressJoi(companyQuerySchema), passport.authenticate('jwt', {session: false}), companyController.get)
-    .post(passport.authenticate('jwt', {session: false}), companyController.create)
-    .put(passport.authenticate('jwt', {session: false}), companyController.update)
-    .delete(passport.authenticate('jwt', {session: false}), companyController.remove);
+    .get(expressJoi(companyQuerySchema), passport.authenticate('jwt', {session: false}), permissionsCheck(routesPermissions.companies), companyController.get)
+    .post(passport.authenticate('jwt', {session: false}), permissionsCheck(routesPermissions.companies), companyController.create)
+    .put(passport.authenticate('jwt', {session: false}), permissionsCheck(routesPermissions.companies), companyController.update)
+    .delete(passport.authenticate('jwt', {session: false}), permissionsCheck(routesPermissions.companies), companyController.remove);
 
 module.exports = router;
--- src/routing/warehouseRoutes/index.js
+++ src/routing/warehouseRoutes/index.js
@@ -5,13 +5,15 @@ const expressJoi = require('express-joi-validator');
 const passport = require('passport');
 const warehouseQuerySchema = require('./ValidatorSchema');
 const warehouseController = require('../../controllers/warehouseController');
+const permissionsCheck = require('../../utils/permissionsValidator');
+const routesPermissions = require('../../const/routesPermissions');
 
 const router = express.Router();
 
 router.route('/')
-    .get(expressJoi(warehouseQuerySchema), passport.authenticate('jwt', {session: false}), warehouseController.get)
-    .post(passport.authenticate('jwt', {session: false}), warehouseController.create)
-    .put(passport.authenticate('jwt', {session: false}), warehouseController.update)
-    .delete(passport.authenticate('jwt', {session: false}), warehouseController.remove);
+    .get(expressJoi(warehouseQuerySchema), passport.authenticate('jwt', {session: false}), permissionsCheck(routesPermissions.warehouses), warehouseController.get)
+    .post(passport.authenticate('jwt', {session: false}), permissionsCheck(routesPermissions.warehouses), warehouseController.create)
+    .put(passport.authenticate('jwt', {session: false}), permissionsCheck(routesPermissions.warehouses), warehouseController.update)
+    .delete(passport.authenticate('jwt', {session: false}), permissionsCheck(routesPermissions.warehouses), warehouseController.remove);
 
 module.exports = router;
